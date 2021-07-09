e05eaedaff554a4dae00e68066be8619a39bf098
Tony Silva <tonysilva@MacBook-Air-de-Tony.local>
2021-07-09 11:32:56 -0300

    adasda

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("apibdp","bb40e74f3e6d82","heroku_e731d5877c22f27",{
+const sequelize = new Sequelize("heroku_e731d5877c22f27","bb40e74f3e6d82","0c091767",{
     host:"us-cdbr-east-04.cleardb.com",
     dialect:"mysql",
     raw:true,
